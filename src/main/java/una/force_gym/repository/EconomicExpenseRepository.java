fd694b58fe257329dfb3465148aeee3b4fbde3cf
Kevin7819 <kvenegasbermudez@gmail.com>
2025-10-20 18:48:54 -0600

    temporal change

--- src/main/java/una/force_gym/repository/EconomicExpenseRepository.java
+++ src/main/java/una/force_gym/repository/EconomicExpenseRepository.java
@@ -10,13 +10,12 @@ import una.force_gym.domain.EconomicExpense;
 
 public interface EconomicExpenseRepository extends JpaRepository<EconomicExpense, Long>{
     
-    @Procedure(procedureName = "prInsertEconomicExpense", outputParameterName = "result")
+    @Procedure(procedureName = "prInsertEconomicExpense")
     int addEconomicExpense(@Param("pIdUser") Long pIdUser, @Param("pRegistrationDate") LocalDate pRegistrationDate, @Param("pVoucherNumber") String pVoucherNumber, @Param("pDetail") String pDetail, @Param("pIdMeanOfPayment") Long pIdMeanOfPayment, @Param("pIdCategory") Long pIdCategory, @Param("pAmount") Float pAmount, @Param("pLoggedIdUser") Long pLoggedIdUser);
 
-    @Procedure(procedureName = "prUpdateEconomicExpense", outputParameterName = "result")
+    @Procedure(procedureName = "prUpdateEconomicExpense")
     int updateEconomicExpense(@Param("pIdEconomicExpense") Long pIdEconomicExpense, @Param("pIdUser") Long pIdUser, @Param("pRegistrationDate") LocalDate pRegistrationDate, @Param("pVoucherNumber") String pVoucherNumber, @Param("pDetail") String pDetail, @Param("pIdMeanOfPayment") Long pIdMeanOfPayment, @Param("pIdCategory") Long pIdCategory, @Param("pAmount") Float pAmount, @Param("pIsDeleted") Long pIsDeleted,  @Param("pLoggedIdUser") Long pLoggedIdUser);
 
     @Procedure(procedureName = "prDeleteEconomicExpense")
     int deleteEconomicExpense(@Param("pIdEconomicExpense") Long pIdEconomicExpense, @Param("pLoggedIdUser") Long pLoggedIdUser);
-    
 }
